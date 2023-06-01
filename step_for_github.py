082dbe09a45c59a9204da930bd9fa062f0d3f1f3
smsuliman <suhairin.muhamadsuliman@dyson.com>
2023-06-01 10:12:50 +0800

    New file for step-by-step push to GitHub

---
+++ step_for_github.py
@@ -0,0 +1,6 @@
+print('git init' +
+'git add README.md' +
+'git commit -m "first commit"' +
+'git branch -M main' +
+'git remote add origin https://github.com/mnhms/lala.git' +
+'git push -u origin main')
